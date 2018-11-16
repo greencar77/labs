6fcd65d2dbd75709e287c08dba7a94524de0a2e5
greencar77 <karlisc@inbox.lv>
2018-11-16 11:04:13 +0200

    streams own stream

---
+++ lab/streams/initial/streams/src/main/java/labs/streams/domain/Group.java
@@ -0,0 +1,15 @@
+package labs.streams.domain;
+
+import java.util.List;
+
+public class Group {
+    private List<Person> persons;
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+}
